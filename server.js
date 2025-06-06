ce9d4765fd72fa2fcf34c14e3130cdd56cf4332c
vimal raj <ervimalraj96@gmail.com>
2025-06-06 21:25:47 +0530

    mounted route

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", jobRoutes);
+app.use("/set",setRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
